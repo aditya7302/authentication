72605024dbe5a72bddbfa5b2231f6574b9399392
Aditya Garg <110886184+aditya7302@users.noreply.github.com>
2023-02-05 19:55:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,71 @@
+if(process.env.NODE_ENV !== "production"){
+    require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt"); 
+const passport = require("passport");
+const initializePassport = require("./passport-config");
+const flash = require("express-flash");
+const session = require("express-session");
+
+initializePassport(passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+    )
+
+const users = [];
+
+app.use(express.urlencoded({extended: false}))
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave :false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session())
+
+app.post("/login",passport.authenticate("local",{
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+}))
+
+app.post("/register",async (req,res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+        console.log(users);
+        res.redirect("/login");
+    } catch (e){
+        console.log(e);
+        res.redirect("/register");
+    }
+})
+
+//routes
+app.get("/",function(req,res){
+    res.render("index.ejs");
+})
+
+app.get("/login",function(req,res){
+    res.render("login.ejs");
+})
+
+app.get("/register",function(req,res){
+    res.render("register.ejs");
+})
+//end routes
+
+
+
+app.listen("3000",function(req,res){
+    console.log("the server is running on port 3000");
+})
